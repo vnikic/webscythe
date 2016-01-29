63a59dd2925658eac69b321d791941ba2d804cb1
vniikic <vnikic@gmail.com>
2016-01-29 19:15:13 +0100

    Basic updates

--- src/org/webscythe/script/msomborac.js
+++ src/org/webscythe/script/msomborac.js
@@ -1,15 +1,27 @@
 var wb = new WebBrowser();
 var sys = new System();
 
-var w = wb.createWindow("http://www.blic.rs/", {javascriptenabled:false});
+var w = wb.createWindow("http://www.blic.rs/");
 
 w.evaluate(function() {
     document.location.href = document.querySelectorAll("#blic_naslovna_print")[1].href;
 });
 
-for (var i = 1; i <= 1; i++) {
+for (var i = 1; i <= 10; i++) {
     var imgUrl = w.evaluate(function() {
         return document.querySelector(".mainPhotoImg").src;
     });
     w.download(imgUrl, "c:/temp/webscythe/somborac" + i + ".jpg");
+
+    var hasNextPic = w.evaluate(function() {
+        var all = document.querySelectorAll('a[href*="blic-strip-crta-i-pise-marko-somborac"]');
+        if (all.length > 1) {
+            document.location.href = all[1].href;
+            return true;
+        }
+        return false;
+    });
+    if (!hasNextPic) {
+        break;
+    }
 }
--- src/org/webscythe/wb/PhantomBrowser.java
+++ src/org/webscythe/wb/PhantomBrowser.java
@@ -122,6 +122,8 @@ public class PhantomBrowser {
         connIn.close();
         fileOut.flush();
         fileOut.close();
+
+        logger.info("Resource downloaded and saved as \"" + fileName + " \".");
     }
 
     public String render(String pageId, String fileName, String type, String rect) {
